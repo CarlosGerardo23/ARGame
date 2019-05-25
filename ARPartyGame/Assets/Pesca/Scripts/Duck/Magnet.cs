df6a29cad0f8f8f06c368ea3ab2d192f338aa20c
CarlosGerardo24 <cenriquez@revartsstudio.com>
2019-05-25 10:44:03 -0500

    Vuforia, arreglo de movimiento

--- ARPartyGame/Assets/Pesca/Scripts/Duck/Magnet.cs
+++ ARPartyGame/Assets/Pesca/Scripts/Duck/Magnet.cs
@@ -4,34 +4,35 @@ using UnityEngine;
 
 public class Magnet : MonoBehaviour
 {
-    [SerializeField] RopeBehaviour rope;
-    [SerializeField] GameObject point;
-    [SerializeField] float distance;
-    private void Start()
+  [SerializeField] RopeBehaviour rope;
+  [SerializeField] GameObject point;
+  [SerializeField] float distance;
+  private void Start()
+  {
+
+  }
+
+  public void Update()
+  {
+    Cathed();
+
+  }
+
+  public void Cathed()
+  {
+    if (Vector3.Distance(point.transform.position, transform.position) < distance)
     {
-        
+      iTween.MoveTo(gameObject, point.transform.position, .1f);
+      //transform.parent = point.transform;
+      StartCoroutine(DestroyObject());
     }
 
-    public void Update()
-    {
-        Cathed();
+  }
+  IEnumerator DestroyObject()
+  {
+    yield return new WaitForSeconds(1f);
+    Destroy(gameObject);
 
-    }
-
-    public  void Cathed()
-    {
-        if(Vector3.Distance(point.transform.position,transform.position)<distance)
-        {
-            iTween.MoveTo(gameObject, point.transform.position, .1f);
-            StartCoroutine(DestroyObject());
-        }
-           
-    }
-    IEnumerator DestroyObject()
-    {
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject);
-
-    }
+  }
 
 }
--- ARPartyGame/Assets/Pesca/Scripts/Rope/RopeMovement.cs
+++ ARPartyGame/Assets/Pesca/Scripts/Rope/RopeMovement.cs
@@ -6,125 +6,129 @@ using UnityEngine;
 public class RopeMovement : MonoBehaviour
 {
 
-    [SerializeField] Vector2 startPosition;
-    [SerializeField] Vector2 lastPosition;
-    [SerializeField] float limitPositionX;
-    [SerializeField] float limitPositionY;
-    [SerializeField] float swipeMinimumMovement;
-    [SerializeField] float speed;
+  [SerializeField] Vector2 startPosition;
+  [SerializeField] Vector2 lastPosition;
+  [SerializeField] float limitPositionX;
+  [SerializeField] float limitPositionY;
+  [SerializeField] float swipeMinimumMovement;
+  [SerializeField] float speed;
 
-    public Vector2 direction;
-    public bool pressed;
+  public Vector2 direction;
+  public bool pressed;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        CheckInput();
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    CheckInput();
+  }
 
-    void CheckInput()
-    {
+  void CheckInput()
+  {
 #if UNITY_EDITOR
-        CheckEditor();
+    CheckEditor();
 #endif
 
 #if UNITY_ANDROID
             CheckAndroid();
 #endif
+  }
+
+  private void CheckEditor()
+  {
+    if (pressed)
+    {
+      lastPosition = Input.mousePosition;
+
+      if (lastPosition != startPosition)
+      {
+        CheckDirection();
+        Move();
+      }
+    }
+    else if (Input.GetMouseButtonDown(0))
+    {   
+        pressed = true;
+
+        startPosition = Input.mousePosition;   
     }
 
-    private void CheckEditor()
+    if (Input.GetMouseButtonUp(0))
     {
-        if(pressed)
+      pressed = false;
+      lastPosition = new Vector2(0, 0);
+      direction = new Vector2(0, 0);
+    }
+    
+  }
+
+  private void CheckAndroid()
+  {
+    foreach (Touch touch in Input.touches)
+    {
+      if (touch.phase == TouchPhase.Began)
+      {
+        startPosition = touch.position;
+        lastPosition = touch.position;
+      }
+
+      if (touch.phase == TouchPhase.Moved)
+      {
+        lastPosition = touch.position;
+        if (lastPosition != startPosition)
         {
-            lastPosition = Input.mousePosition;
-
-            if (lastPosition != startPosition )
-            {
-                CheckDirection();
-                Move();
-            }
-        }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-                pressed = true;
-            else
-                startPosition = Input.mousePosition;
+          CheckDirection();
+          Move();
         }
 
-        if (Input.GetMouseButtonUp(0))
-            pressed = false;
+      }
+    }
+  }
+
+  float DistancePositions()
+  {
+    float result = Vector2.Distance(lastPosition, startPosition);
+    return result;
+  }
+  private void CheckDirection()
+  {
+    if (MoveHorizontalTotal() > swipeMinimumMovement)
+    {
+      if (lastPosition.x - startPosition.x > 0)
+        direction = Vector2.right;
+      else if (lastPosition.x - startPosition.x < 0)
+        direction = Vector2.left;
+    }
+    else if (MoveVertcalTotal() > swipeMinimumMovement)
+    {
+      if (lastPosition.y - startPosition.y > 0)
+        direction = Vector2.up;
+      else if (lastPosition.y - startPosition.y < 0)
+        direction = Vector2.down;
     }
 
-    private void CheckAndroid()
-    {
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase == TouchPhase.Began)
-            {
-                startPosition = touch.position;
-                lastPosition = touch.position;
-            }
 
-            if (touch.phase == TouchPhase.Moved)
-            {
-                lastPosition = touch.position;
-                if (lastPosition != startPosition )
-                {
-                    CheckDirection();
-                    Move();
-                }
 
-            }
-        }
-    }
+  }
+  float MoveHorizontalTotal()
+  {
+    return Mathf.Abs(lastPosition.x - startPosition.x);
+  }
+  float MoveVertcalTotal()
+  {
+    return Mathf.Abs(lastPosition.y - startPosition.y);
+  }
+  private void Move()
+  {
+    if (direction == Vector2.right && transform.localPosition.x < limitPositionX)
+      transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
+    if (direction == Vector2.left && transform.localPosition.x > -limitPositionX)
+      transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
+    if (direction == Vector2.up && transform.localPosition.z < limitPositionY)
+      transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
+    if (direction == Vector2.down && transform.localPosition.z > limitPositionY - 3)
+      transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
 
-    float DistancePositions()
-    {
-        float result = Vector2.Distance(lastPosition, startPosition);
-        return result;
-    }
-    private void CheckDirection()
-    {
-        if(MoveHorizontalTotal()> swipeMinimumMovement)
-        {
-            if (lastPosition.x - startPosition.x > 0)
-                direction = Vector2.right;
-            else if (lastPosition.x - startPosition.x < 0)
-                direction = Vector2.left;
-        }
-        else if(MoveVertcalTotal() > swipeMinimumMovement)
-        {
-            if (lastPosition.y - startPosition.y > 0)
-                direction = Vector2.up;
-            else if (lastPosition.y - startPosition.y < 0)
-                direction = Vector2.down;
-        }
-        
-         
 
-    }
-    float MoveHorizontalTotal()
-    {
-        return Mathf.Abs(lastPosition.x - startPosition.x);
-    }
-    float MoveVertcalTotal()
-    {
-        return Mathf.Abs(lastPosition.y - startPosition.y);
-    }
-    private void Move()
-    {
-        if (direction == Vector2.right&& transform.localPosition.x < limitPositionX)
-            transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
-        if (direction == Vector2.left&& transform.localPosition.x > -limitPositionX)
-            transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
-        if (direction == Vector2.up&& transform.localPosition.z < limitPositionY)
-            transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
-        if (direction == Vector2.down&&transform.localPosition.z > limitPositionY-3)
-            transform.Translate(new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime);
-
-       
-    }
+  }
 }
